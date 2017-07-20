8b86574d1e208d3536976709cfb5b571efa10867
LAPTOP-UKICOT5O\skysy <skysy56@gmail.com>
2017-07-20 22:32:37 +0900

    최초

---
+++ src/yamoyeo/GroupListController.java
@@ -0,0 +1,44 @@
+package yamoyeo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class GroupListController {
+
+	@RequestMapping(value = "groupList.do")
+	public void groupList(HttpServletRequest request, HttpServletResponse response, HttpSession session)
+			throws ServletException, IOException { // 내 그룹 리스트 출력
+		// Parameter
+		String user_id = (String) session.getAttribute("user_id");
+		
+		// user 확인
+		if (user_id.isEmpty()) {
+			request.setAttribute("error", "로그인 해주세요.");
+			HttpUtil.forward(request, response, "/view01_d.jsp");
+			return;
+		}
+
+		// Service
+		Service service = Service.getInstance();
+		ArrayList<GroupVO> list = service.searchGroups(user_id);
+
+		// Output View
+		if (list == null) {
+			request.setAttribute("result", "가입된 모임이 없습니다.");
+		}
+		for(int i = 0; i<list.size(); i++){
+			request.setAttribute("mygroup"+i, list.get(i));
+		}
+		HttpUtil.forward(request, response, "/view08_d.jsp");
+	}
+
+
+}
---
+++ src/yamoyeo/GroupSearchController.java
@@ -0,0 +1,76 @@
+package yamoyeo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class GroupSearchController {
+
+	@RequestMapping(value = "groupSearch.do")
+	public void searchGroup(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Parameter
+		HttpSession session = request.getSession();
+		String group_id = request.getParameter("group_id");
+		String user_id = (String) session.getAttribute("user_id");
+		ArrayList<CommentVO> list = null;
+		// Service
+		Service service = Service.getInstance();
+		GroupVO group = service.searchGroup(group_id);
+		int join_state = service.searchJoin(group_id, user_id);
+		System.out.println(join_state);
+		// Output View
+		request.setAttribute("detailImg", "c:/logs/" + group.getImage());
+		request.setAttribute("group", group);
+		request.setAttribute("join_state", join_state);
+
+		list = service.getComment(request.getParameter("group_id"));
+		for (int i = 0; i < list.size(); i++) {
+			request.setAttribute("comment" + i, list.get(i));
+		}
+
+		HttpUtil.forward(request, response, "/view04_d.jsp");
+	}
+
+	@RequestMapping(value = "searchGroup.do")
+	public String groupSearch(HttpServletRequest request, HttpServletResponse response) {
+
+		System.out.println("searchGroup.do 해서 들어왔나");
+		String interest = request.getParameter("interest");
+		String address = request.getParameter("address");
+		String day = request.getParameter("day");
+
+		System.out.println("받아온 관심사, 지역, 요일 = " + interest + ", " + address + ", " + day);
+		Service service = Service.getInstance();
+		ArrayList<GroupVO> list = service.groupSearch(interest, address, day);
+		for (int i = 0; i < list.size(); i++) {
+			request.setAttribute("group" + i, list.get(i));
+			request.setAttribute("searchImg"+i,"c:/logs/"+list.get(i).getImage());
+		}
+
+		return "view03_d";
+	}
+
+	@RequestMapping(value = "searchMygroup.do")
+	public String searchMygroup(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		String user_id = (String) session.getAttribute("user_id");
+		ArrayList<GroupVO> list = null;
+		Service service = Service.getInstance();
+		list = service.searchMygroup(user_id);
+
+		for (int i = 0; i < list.size(); i++) {
+			request.setAttribute("group" + i, list.get(i));
+		}
+
+		return "view08_d";
+	}
+
+}
